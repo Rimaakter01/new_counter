88a21f0e9e7609897aa52b18071f51dd92c00b8e
Rimaakter01 <158799517+Rimaakter01@users.noreply.github.com>
2024-03-24 11:14:36 +0600

    Add files via upload

---
+++ count.js
@@ -0,0 +1,21 @@
+
+let x = 0;
+
+function cd(){
+    x++;
+document.getElementById('counter_value').innerHTML = x;
+
+}
+
+function ab(){
+    x--;
+    document.getElementById('counter_value').innerHTML = x;
+   
+    }
+
+    function bc(){
+        x = 0;
+        document.getElementById('counter_value').innerHTML = x;
+       
+        }
+
